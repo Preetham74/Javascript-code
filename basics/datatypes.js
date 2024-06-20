de26f8a0a1c06e4f3e8a8befc9eda11c79be4ccc
Preetham74 <133340856+Preetham74@users.noreply.github.com>
2024-06-20 06:47:28 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+let age = 18
+let isloggedin = false
+let state = "Bengaluru"
+
+//primitive datatypes
+//bigint
+//number
+//string
+//boolean
+//null = standalone value   typeof null will give object
+//undefined                 typeof undefined will give undefined
+//symbol = unique
