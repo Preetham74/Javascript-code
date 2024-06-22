7bfcd6011ccb75ca75e2a5c77b8656e087a4df52
Preetham74 <133340856+Preetham74@users.noreply.github.com>
2024-06-22 13:33:28 +0000

    objects

---
+++ basics/objects.js
@@ -0,0 +1,10 @@
+const obj ={
+    name : "Aizen",
+    "fullname" : "Aizen Sosuke",
+    age : 24,
+    email : "aizen@google.com",
+    city : "Bengaluru"
+}
+console.log(obj.age)
+console.log(obj.email)
+console.log(obj["email"])
