c454167539898203243db0ad5440e2219fea4362
Preetham74 <133340856+Preetham74@users.noreply.github.com>
2024-06-22 12:44:58 +0000

    updated

--- basics/strings.js
+++ basics/strings.js
@@ -1,6 +1,8 @@
 let name = "Preetham"
 let count = 50
 console.log(`Hello my name is ${name} and my repo count is ${count}`)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "3")
 
 //let any = new String('Preetham')
 //another way of defining stings.
